f17bf1358382cea3b829052c45083473cb823010
Andrés Rodríguez <andrudeluxe@gmail.com>
2024-02-09 19:51:40 -0500

    register pages functionality created

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,31 +1,81 @@
+"use client";
+
 import Button from "@/components/button/Button";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-const page = () => {
+const SignIn = () => {
+  const router = useRouter();
+
+  const [error, setError] = useState<string>("");
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    return setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (!user.email || !user.password) {
+        setError("Please fill all the fields");
+        return;
+      }
+
+      await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      }).then(async (res) => {
+        if (res.status === 200 || res.status === 201) {
+          router.push("/");
+          setError("");
+        }
+      });
+    } catch {
+      setError("Something went wrong");
+    } finally {
+      setUser({
+        email: "",
+        password: "",
+      });
+    }
+  };
+
   return (
     <main className="main-page-section flex justify-center items-center min-h-screen">
-      <form className="bg-light flex flex-col gap-y-4 items-center border border-light-3 py-14 px-10 rounded-xl mb-8 max-w-[400px] shadow shadow-gray-1/30">
+      <form
+        className="bg-light flex flex-col gap-y-4 items-center border border-light-3 py-14 px-10 rounded-xl mb-8 min-w-[320px] max-w-[400px] shadow shadow-gray-1/30"
+        onSubmit={handleSubmit}
+      >
         <UserCircleIcon className="w-10 h-10 text-primary" />
         <h2 className="text-xl font-gray-1 font-medium">Sign In</h2>
         <input
           type="email"
           placeholder="Email"
+          name="email"
+          value={user.email}
+          onChange={handleInputChange}
           className="input-register-form mt-4"
         />
-        <div className="flex gap-2">
-          <input
-            type="password"
-            placeholder="Password"
-            className="input-register-form"
-          />
-          <input
-            type="password"
-            placeholder="Confirm password"
-            className="input-register-form"
-          />
-        </div>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={user.password}
+          onChange={handleInputChange}
+          className="input-register-form"
+        />
         <Button text="Sign In" className="w-full mt-4" />
         <div className="flex gap-2 items-center w-full">
           <hr className="border-light-3 w-full" />
@@ -48,15 +98,16 @@ const page = () => {
           outline
         />
         <p className="text-sm text-gray-2">
-          Don&lsquo;t you have an account?{" "}
+          Don&lsquo;t have an account?{" "}
           <Link href="/signup" className="text-primary font-medium">
             Sign Up
           </Link>
           .
         </p>
+        {error !== "" && <p className="error-span">{error}</p>}
       </form>
     </main>
   );
 };
 
-export default page;
+export default SignIn;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,33 +1,101 @@
+"use client";
+
+import { useState } from "react";
+
 import Button from "@/components/button/Button";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const SignUp = () => {
+  const router = useRouter();
+
+  const [error, setError] = useState<string>("");
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    return setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (!user.name || !user.username || !user.email || !user.password) {
+        setError("Please fill all the fields");
+        return;
+      }
+
+      await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      }).then(async (res) => {
+        if (res.status === 200 || res.status === 201) {
+          router.push("/");
+          setError("");
+        }
+      });
+    } catch {
+      setError("Something went wrong");
+    } finally {
+      setUser({
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+      });
+    }
+  };
+
   return (
     <main className="main-page-section flex justify-center items-center min-h-screen">
-      <form className="bg-light flex flex-col gap-y-4 items-center border border-light-3 py-14 px-10 rounded-xl mb-8 max-w-[400px] shadow shadow-gray-1/30">
+      <form
+        className="bg-light flex flex-col gap-y-4 items-center border border-light-3 py-14 px-10 rounded-xl mb-8 max-w-[400px] shadow shadow-gray-1/30"
+        onSubmit={handleSubmit}
+      >
         <UserCircleIcon className="w-10 h-10 text-primary" />
         <h2 className="text-xl font-gray-1 font-medium">Sign Up</h2>
         <input
           type="text"
           placeholder="Name"
+          name="name"
+          value={user.name}
+          onChange={handleInputChange}
           className="input-register-form mt-4"
         />
         <input
           type="text"
           placeholder="Username"
+          name="username"
+          value={user.username}
+          onChange={handleInputChange}
           className="input-register-form"
         />
         <input
           type="email"
           placeholder="Email"
+          name="email"
+          value={user.email}
+          onChange={handleInputChange}
           className="input-register-form"
         />
         <div className="flex gap-2">
           <input
             type="password"
             placeholder="Password"
+            name="password"
+            value={user.password}
+            onChange={handleInputChange}
             className="input-register-form"
           />
           <input
@@ -64,9 +132,10 @@ const page = () => {
           </Link>
           .
         </p>
+        {error !== "" && <p className="error-span">{error}</p>}
       </form>
     </main>
   );
 };
 
-export default page;
+export default SignUp;
